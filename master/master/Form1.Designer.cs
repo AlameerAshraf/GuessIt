f9c58d3f6c1b651e72e15a32034dc866b235cb0a
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-02-16 23:14:06 +0200

    Creating Master Server
    Master Server Connect Multiple Clients ,.

---
+++ master/master/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace master
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(609, 293);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(601, 267);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(595, 261);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Player_id";
+            this.columnHeader1.Width = 152;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Player_name";
+            this.columnHeader2.Width = 136;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Player_status";
+            this.columnHeader3.Width = 234;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(601, 267);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ master/master/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+
+
+/* /*
+      async void connection()
+        {
+            Player p1 = new Player(await Started.AcceptSocketAsync(), listView1);
+            players.Add(p1);
+            connection();
+        }
+ /* */
+
+namespace master
+{
+    public partial class Form1 : Form
+    {
+        TcpListener Started;
+        List<Player> players;
+        bool Flag = true;
+        string[] arr; 
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false; 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            byte[] Ip = new byte[] { 127, 0, 0, 1 };
+            IPAddress Iaddress = new IPAddress(Ip);
+            Started = new TcpListener(Iaddress, 9999);
+            Started.Start();
+            Thread TH2 = new Thread(Oops);
+            TH2.Start();
+        }
+        public void Oops()
+        {
+            while (Flag)
+            {
+                Player Pl = new Player(Started.AcceptSocket(), listView1);
+            }  
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            Flag = false; 
+            Started.Stop(); 
+        }
+    }
+
+    public class Player
+    {
+        Thread Th1;
+        Socket Dummy;
+        NetworkStream Stream;
+        BinaryReader WhoReads;
+        BinaryWriter WhoWrites;
+        string PlayersName;
+        string PlayersPassword;
+        string[] Row;
+        ListView PanelControl;
+        
+        public Player(Socket ObjFromSocket , ListView Li  )
+        {
+            Dummy = ObjFromSocket;
+            PanelControl = Li; 
+            Th1 = new Thread(Run);     
+            Th1.Start(); 
+        }
+
+        public bool SocketActive (Socket s)
+        {
+            bool ac1 = s.Poll(1000, SelectMode.SelectRead);
+            bool ac2 = (s.Available == 0);
+            if (ac1 && ac2)
+                return false;
+            else
+                return true;
+        }
+        public void Run()
+        { 
+            Stream = new NetworkStream(Dummy);
+            WhoReads = new BinaryReader(Stream);
+            do
+            {
+                try
+                {
+                    PlayersName = WhoReads.ReadString();
+                    Th1.Name = PlayersName;
+                    MessageBox.Show(PlayersName);
+                    Row = new string[] { "1", PlayersName, "Online" };
+                    var li = new ListViewItem(Row);
+                    PanelControl.Items.Add(li);
+                }
+                catch
+                {
+                    MessageBox.Show("oops");
+                }
+            }
+            while (SocketActive(Dummy));
+
+            foreach (ListViewItem item in PanelControl.Items)
+            {
+                if (item.SubItems[1].Text== PlayersName)
+                {
+                  item.SubItems[2].Text = "Offline";
+                }
+            }
+
+            Dummy.Close(); 
+
+        }
+    }
+}
